7ca96abb5acc38833b7ab423d57b880a8c084ab2
Nordwall <tobiasnordwall@gmail.com>
2022-09-05 23:31:30 +0200

    my first commit

---
+++ FileCrawler.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileCrawler {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Enter a word to search for");
+        String input = inputScanner.nextLine();
+        readFiles(System.getProperty("user.dir"), input);
+        inputScanner.close();
+    }
+
+    private static void readFiles(String Filepath, String input) {
+        
+            File currentDirectory = new File(Filepath);
+            File[] files = currentDirectory.listFiles();
+            Scanner scanner = null; 
+            for (File file : files) {
+                if(file.isDirectory()) {
+                    readFiles(currentDirectory + file.toString(), input);
+                } else {
+                    try {
+                    scanner = new Scanner(file);
+                    String string = scanner.nextLine();
+                    if(string.toLowerCase().contains(input.toLowerCase())) {
+                        System.out.println("\nString found in : " + file.getAbsolutePath());
+                    }
+                } catch(Exception e) {
+                    System.err.println("\nFile or directory could not be read at : " + file.getAbsolutePath());
+                } 
+            } 
+        }
+
+    }
+}
